314d303c399d458d4bde19993cc3e80812e8100b
cutebaby0630 <lovemath0630@gmail.com>
2019-11-22 12:25:13 +0800

    20191122 kendo 介面

---
+++ test.js
@@ -0,0 +1,70 @@
+
+ var data = [
+    { text: "RD", value: "v_RD" },
+    { text: "ICP", value: "v_ICP" },
+    { text: "PENrose", value: "v_PENrose" }
+]
+$(document).ready(function () {
+   
+    $("#start_datepicker").kendoDatePicker({
+        value: new Date(),
+        min: "1900-01-01",
+        max: new Date(),
+        dateInput: true,
+        format: "yyyy-MM-dd"
+    });
+    $("#expiry_datepicker").kendoDatePicker({
+        value: new Date(),
+        min: "1900-01-01",
+        max: new Date(),
+        dateInput: true,
+        format: "yyyy-MM-dd"
+    });
+
+   
+});
+$("#PipelineName").kendoDropDownList({
+    dataTextField: "text",
+    dataValueField: "value",
+    dataSource:data,
+    index: 0,
+    // change: onCategoryChange
+});
+var locatoinX = [];
+var locatoinY = [];
+var intubation = false;
+//讀取資料庫資訊塞進陣列裡面
+var locationPointer = locatoinX.length;
+var img = new Image();
+img.src = "https://i.imgur.com/lQx1bV0.jpg";
+window.onload = function () {
+    var lc = document.getElementById("full");
+    var lctx = lc.getContext("2d");
+    lctx.drawImage(img, 0, 0, 600, 600);
+    lc.onclick = function () {
+        if (intubation) {
+            var x = event.clientX;
+            var y = event.clientY;
+            locatoinX[locationPointer] = x;
+            locatoinY[locationPointer] = y;
+            locationPointer = locationPointer + 1;
+        }
+        for (var i = 0; i < locationPointer; i++) {
+            lctx.beginPath();
+            lctx.arc(locatoinX[i] - 15, locatoinY[i] - 15, 5, 0, 2 * Math.PI);
+            lctx.fill();
+        }
+    };
+}
+
+function add() {
+    intubation = true;
+    var mydateInput = document.getElementById("date");
+    var date = new Date();
+    var dateString = date.getFullYear() + "/" + (date.getMonth() + 1) + "/" + date.getDate();
+    mydateInput.value = dateString;
+}
+
+function save() {
+    intubation = false;
+}
